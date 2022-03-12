7036c0e77d5d64edfe2c26dc365f31fac2291c90
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2022-03-12 15:19:45 +0200

    Add initial project files

---
+++ Quarantine-Finder/src/main/java/com/example/quarantinefinder/QuarantineFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.quarantinefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarantineFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuarantineFinderApplication.class, args);
+    }
+
+}
---
+++ Quarantine-Finder/src/test/java/com/example/quarantinefinder/QuarantineFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quarantinefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarantineFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
