c028dcb0736928402de94104f89875a8a9e6e8b7
hp <hp@DESKTOP-ERG5JO7>
2022-03-12 19:52:20 +0200

    add annotations

--- Quarantine-Finder/src/main/java/com/example/quarantinefinder/rest/PatientRestController.java
+++ Quarantine-Finder/src/main/java/com/example/quarantinefinder/rest/PatientRestController.java
@@ -1,6 +1,8 @@
 package com.example.quarantinefinder.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.quarantinefinder.constant.AbstractRestControllerImpl;
 import com.example.quarantinefinder.criteria.PatientCriteria;
@@ -10,7 +12,8 @@ import com.example.quarantinefinder.response.PatientResponse;
 import com.example.quarantinefinder.service.PatientService;
 
 import lombok.Getter;
-
+@RestController
+@RequestMapping("/patient")
 public class PatientRestController extends AbstractRestControllerImpl<PatientRequest, PatientResponse, PatientCriteria, Patient, Long> {
 
 	@Autowired
